22eac027c18176dacc8c4578a75c8e6f82b65079
zcnguyen <zcnguyen@hatboro-horsham.org>
2026-05-06 14:25:56 -0400

    wrote code for gui

--- branch gui.py
+++ branch gui.py
@@ -1,8 +1,23 @@
 import pygame
-import tkinter as tk
-import tkinter.font as tkFont
-from PIL import Image, ImageTk
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 258))
+clock = pygame.time.Clock()
+running = True
+bg = pygame.image.load('"C:\Users\zcnguyen\Downloads\supermario  1-1 map.png"')
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 
+    screen.fill("black")
 
 
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
