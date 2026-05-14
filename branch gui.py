54551847021c3b5a139312e0af48e10dd3a2e8a8
zcnguyen <zcnguyen@hatboro-horsham.org>
2026-05-14 14:29:27 -0400

    changed nothing

--- branch gui.py
+++ branch gui.py
@@ -1,10 +1,11 @@
 import pygame
-bg_scroll = 0
+scroll = 0
 p1_speed = 20
 p1_gravity = 5
 p1_x = 136
 p1_y = 187
-
+x = 20
+y = 20
 class Player(pygame.sprite.Sprite):
 
 
@@ -15,9 +16,11 @@ screen = pygame.display.set_mode((800, 240))
 clock = pygame.time.Clock()
 running = True
 
+scroll = 0
+scroll = 5
 
 while running:
-    bg = screen.blit(pygame.image.load('mariomap.png').convert_alpha(), (0,0))
+    image = screen.blit(pygame.image.load('mariomap.png').convert_alpha(), (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -33,23 +36,13 @@ while running:
         p1_y += p1_gravity
 
 
-
-
-
-
-
-
-
     p1 = pygame.draw.rect(screen, (150, 75, 0), pygame.Rect(0, 550, 1000, 50))
     pygame.draw.rect(screen, (140, 60, 0), pygame.Rect(400, 320, 50, 50))
     p3 = pygame.draw.circle(screen, (150, 60, 0), (p1_x, p1_y), 8, )
     pygame.display.flip()
     clock.tick(60)
 
-
-
-
-
+    screen.blit()
 
     clock.tick(60)
 
