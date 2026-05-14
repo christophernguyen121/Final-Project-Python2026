dfe6b8757c78de2f859eb1c29dee416a410eee18
zcnguyen <zcnguyen@hatboro-horsham.org>
2026-05-14 19:40:44 -0400

    made backround move and follow player

--- branch gui.py
+++ branch gui.py
@@ -1,49 +1,79 @@
 import pygame
+
+pygame.init()
+
 scroll = 0
-p1_speed = 20
+p1_speed = 5
 p1_gravity = 5
 p1_x = 136
 p1_y = 187
-x = 20
-y = 20
-class Player(pygame.sprite.Sprite):
 
+width = 800
+height = 240
+scroll_area_width = 200
 
-
-
-    pygame.init()
-screen = pygame.display.set_mode((800, 240))
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
-scroll = 0
-scroll = 5
+
+background = pygame.image.load("mariomap.png").convert_alpha()
+
+offset_x = 0
 
 while running:
-    image = screen.blit(pygame.image.load('mariomap.png').convert_alpha(), (0,0))
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen_x = pygame.rect
+
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        p1_x -= p1_gravity
+        p1_x -= p1_speed
     if keys[pygame.K_RIGHT]:
-        p1_x += p1_gravity
+        p1_x += p1_speed
     if keys[pygame.K_UP]:
-        p1_y -= p1_gravity
+        p1_y -= p1_speed
     if keys[pygame.K_DOWN]:
-        p1_y += p1_gravity
+        p1_y += p1_speed
 
 
-    p1 = pygame.draw.rect(screen, (150, 75, 0), pygame.Rect(0, 550, 1000, 50))
-    pygame.draw.rect(screen, (140, 60, 0), pygame.Rect(400, 320, 50, 50))
-    p3 = pygame.draw.circle(screen, (150, 60, 0), (p1_x, p1_y), 8, )
+    screen_player_x = p1_x - offset_x
+
+    if screen_player_x > width - scroll_area_width:
+        offset_x += p1_speed
+
+    if screen_player_x < scroll_area_width:
+        offset_x -= p1_speed
+
+
+    screen.blit(background, (-offset_x, 0))
+
+
+    pygame.draw.rect(
+        screen,
+        (150, 75, 0),
+        pygame.Rect(-offset_x, 200, 2000, 40)
+    )
+
+
+    pygame.draw.rect(
+        screen,
+        (140, 60, 0),
+        pygame.Rect(400 - offset_x, 150, 50, 50)
+    )
+
+    
+    pygame.draw.circle(
+        screen,
+        (150, 60, 0),
+        (p1_x - offset_x, p1_y),
+        8
+    )
+
     pygame.display.flip()
-    clock.tick(60)
-
-    screen.blit()
-
     clock.tick(60)
 
 pygame.quit()
